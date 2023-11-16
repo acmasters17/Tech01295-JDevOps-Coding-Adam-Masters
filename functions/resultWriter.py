c9f006a95a38e218f43de15968b5fefcddfeeca8
Adam Masters <74498986+acmasters17@users.noreply.github.com>
2023-11-16 14:15:40 +0000

    Added comments to result writer

--- functions/resultWriter.py
+++ functions/resultWriter.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 import json
 
-# function to write results out to a named json
+# function to write results out to a named json thats constructed from city and the time the program was run
 def writeResultsToFile(city:str,temperatureResults):
     print("Writing to file...")
     FILENAME = city.lower() + "_" + datetime.now().strftime("%Y_%m_%d_T%H:%M:%SZ") + ".json"
     file = open("./outputs/" + FILENAME,"w")
     json.dump(temperatureResults,file)
-    print("Finished writing to file - ",FILENAME)
+    print("Finished writing to file -",FILENAME)
 
 
 
